9f5c40a5c2650c56f76f7a07acc17f9e3680c48e
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-02-14 17:55:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ def find_mismatch(text):
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            opening_brackets_stack[-1].match(next):
-            print(i+1)
-            exit()
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next):
+                return i+1
+            opening_brackets_stack.pop()
+                
             pass
         
     if not opening_brackets_stack:
