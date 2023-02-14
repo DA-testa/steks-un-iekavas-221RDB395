18fae501b9b4b636f8b8d9e218eff8a9cb995083
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-02-14 18:21:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # python3
+# Anastasija Bondare, 221RDB395, 13.grupa
 
 from collections import namedtuple
 
@@ -14,21 +15,20 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            opening_brackets_stack.append(Bracket(next,i+1))
+            opening_brackets_stack.append(Bracket(next,i+1)) # pievieno klāt nākamo vērtību (tikai vienu)
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next):
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next): # iet pa vienu vienību pa kreisi
                 return i+1
-            opening_brackets_stack.pop()
-                
+            opening_brackets_stack.pop()     
             pass
         
     if not opening_brackets_stack:
         print("Success")
     else:
-        print(opening_brackets_stack[0].position)
+        return opening_brackets_stack[0].position
         
 
 def main():
