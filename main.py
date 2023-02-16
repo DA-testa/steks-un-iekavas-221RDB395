14cb435f03bdf5e9aced688acc1f8cc35c3033e1
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-02-16 15:43:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,34 +15,27 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next,i+1)) # pievieno klāt nākamo vērtību (tikai vienu)
-            pass
+            
+
 
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next): # iet pa vienu vienību pa kreisi
                 return i+1
             opening_brackets_stack.pop()   
-            pass
-
-    if not opening_brackets_stack:
-            print("Success")
-        else:
-            print(opening_brackets_stack[0].position)
-
             
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next): # iet pa vienu vienību pa kreisi
-                return i+1
-            opening_brackets_stack.pop()     
-            pass
-        
+
+
     if not opening_brackets_stack:
-        print("Success")
+        return "Success"
     else:
-        print(opening_brackets_stack[0].position)
+        return opening_brackets_stack[0].position
+
 
 
 def main():
     text = input()
-    if i in text:
+    if "I" in text:
+        text = input()
         mismatch = find_mismatch(text)
         print(mismatch) #izvade 
 
