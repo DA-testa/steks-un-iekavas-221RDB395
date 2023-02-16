1fe173bf17dc6893cd07887a2e78495629dc1cf1
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-02-16 13:22:23 +0000

    -

--- main.py
+++ main.py
@@ -15,18 +15,21 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next,i+1)) # pievieno klāt nākamo vērtību (tikai vienu)
-            pass
+            
+
 
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next): # iet pa vienu vienību pa kreisi
                 return i+1
             opening_brackets_stack.pop()   
-            pass
+            
+
 
     if not opening_brackets_stack:
-            print("Success")
-        else:
-            print(opening_brackets_stack[0].position)
+        return "Success"
+    else:
+        return opening_brackets_stack[0].position
+
 
 
 def main():
